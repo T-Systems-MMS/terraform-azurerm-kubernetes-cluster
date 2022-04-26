15f49c7656ef9e67e7aa8dd9b8a0a109b48b098a
mima <Michaela.Mattes@t-systems.com>
2022-04-26 15:07:41 +0200

    change for provider azurerm 3.x version

--- versions.tf
+++ versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     azurerm = {
       source  = "registry.terraform.io/hashicorp/azurerm"
-      version = "~>2.79"
+      version = ">=3.3.0"
     }
   }
-  required_version = "~>1.0"
+  required_version = "~>1.1"
 }
